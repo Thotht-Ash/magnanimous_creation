3b685cb93509bf65dc7ceaeae404e15ba9baef6f
Bjarne Klaaßen <bjarnekam@web.de>
2021-02-11 15:16:46 +0100

    works, added some banned words i could think of

---
+++ magnanimous_creation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3.9
+import requests
+import random
+from app_keys import app_id, app_key
+banned_words = ["cancer", 'mutilated', "molested", "cunt", "chink",
+                "miscarriage", "cuck", "cock", "cuckold"]
+
+base_url_m = \
+    "https://od-api.oxforddictionaries.com/api/v2/search/thesaurus/en?q=m&" + \
+    "prefix=true"
+base_url_c = \
+    "https://od-api.oxforddictionaries.com/api/v2/search/thesaurus/en?q=c&" + \
+    "prefix=true"
+
+rm = requests.get(base_url_m, headers={"app_id": app_id, "app_key": app_key})
+rc = requests.get(base_url_c, headers={"app_id": app_id, "app_key": app_key})
+
+word_m = random.choice(rm.json()['results'])['word']
+while " " in word_m or "-" in word_m or word_m in banned_words:
+    word_m = random.choice(rm.json()['results'])['word']
+
+word_c = random.choice(rc.json()['results'])['word']
+while " " in word_c or "-" in word_c or word_c in banned_words:
+    word_c = random.choice(rc.json()['results'])['word']
+
+print(word_m + " " + word_c)
